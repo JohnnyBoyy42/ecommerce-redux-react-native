6cb5426639830a31b72015dfbbecd642f912d2ca
Jonas <jonaslagoteixeira@gmail.com>
2019-12-12 19:25:58 -0300

    feito configuracao do json-server

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 
 import { Container, AddToCartButton, AddToCartText } from './styles';
 
@@ -11,7 +12,20 @@ export default class Main extends Component {
   };
 
   async componentDidMount() {
-    const response = await api.get('products');
+    try {
+      const response = await api.get('products');
+
+      const data = response.data.map(product => ({
+        ...product,
+        priceFormatted: product.price,
+      }));
+
+      console.tron.log(data);
+
+      this.setState({ products: response.data });
+    } catch (error) {
+      console.tron.log(error);
+    }
   }
 
   render() {
--- src/util/format.js
+++ src/util/format.js
@@ -1,4 +1,6 @@
-export const { format: formatPrice } = new Intl.NumberFormat('pt-BR', {
-  style: 'currency',
-  currency: 'BRL',
-});
+// import Intl from 'react-native-intl';
+
+// export const { format: formatPrice } = new Intl.NumberFormat('pt-BR', {
+//   style: 'currency',
+//   currency: 'BRL',
+// });
