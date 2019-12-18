221cf54fa73fc4814ef8658e2136702152737877
Jonas <jonaslagoteixeira@gmail.com>
2019-12-18 16:06:04 -0300

    feito configuracao do navigation no saga

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,4 +1,5 @@
 import { all, takeLatest, put } from 'redux-saga/effects';
+import NavigationService from '../../../services/navigation';
 
 import { addToCartRequestSucess } from './actions';
 
@@ -6,6 +7,8 @@ function* addToCart({ id }) {
   console.tron.log(id, 'TO NO SAGA');
 
   yield put(addToCartRequestSucess(id));
+
+  NavigationService.navigate('Cart');
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
